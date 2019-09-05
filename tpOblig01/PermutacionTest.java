e88d828e4550f7fe77e97f29e917fd80ffe0ce06
liamosycka <osyckaliam@gmail.com>
2019-09-05 19:15:15 -0300

    agregando nuevos test cases

--- tpOblig01/PermutacionTest.java
+++ tpOblig01/PermutacionTest.java
@@ -12,14 +12,18 @@ class PermutacionTest {
 		arr1=RandomArrLetras.generarArreglo(5);
 		arr2=RandomArrLetras.generarArreglo(6);
 		assertFalse(Permutacion.sonPermutacion(arr1, arr2));
+		arr1=RandomArrLetras.generarArreglo(1000000);
+		arr2=RandomArrLetras.generarArregloPermutacion(arr1);
+		assertTrue(Permutacion.sonPermutacion(arr1, arr2));
 		char[]arr3= {'a','b','c','c','c'};
 		char[]arr4= {'c','c','c','c','a'};
 		assertFalse(Permutacion.sonPermutacion(arr3, arr4));
 		char[] arr5= {'a','d','c','d','d','a','b','c','c','c'};
 		char[] arr6= {'c','a','c','c','c','a','b','d','d','d'};
 		assertTrue(Permutacion.sonPermutacion(arr5, arr6));
-		
-		
+		char[] arr7= {'a'};
+		char[] arr8= {'a'};
+		assertTrue(Permutacion.sonPermutacion(arr7, arr8));
 		
 	}
 
